713862e2c1c75fc2a83393c337d6b16315dccfd6
Eduardo Filip <filip_eduardo@hotmail.com>
2024-05-31 00:35:46 -0300

    Implementação de Clock

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -5,32 +5,42 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.visit('./src/index.html')
     })
 
+    // Variáveis
+
+    const   skipTime = 3000
+
     /* TC001 - Verificar o Título da aplicação */
 
     it('TC001 - Verificar o Título da aplicação', function() {
         cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
+   
     })
 
     /* TC002 - Preenche os campos obrigatórios e envia o formulário */
 
-    it ('TC002 - Preenche os campos obrigatórios e envia o formulário', function() {
+    it('TC002 - Preenche os campos obrigatórios e envia o formulário', function() {
+    cy.clock()
     cy.fillMandatoryFieldsAndSubmit()
-
     cy.get('.success').should('be.visible')
+    cy.tick(skipTime)
+    cy.get('.success').should('not.be.visible')
 
     })
 
     /* TC003 - Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida */
 
-    it ('TC003 - Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function () {
+    it('TC003 - Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function () {
 
+    cy.clock()
     cy.get('#firstName').type('Eduardo')
     cy.get('#lastName').type('Filip')
     cy.get('#email').type('teste.email.com')
     cy.get('#open-text-area').type('Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste', {delay: 0})
     cy.get('button[type="submit"]').click()
-
+    
     cy.get('.error').should('be.visible')
+    cy.tick(skipTime)
+    cy.get('.error').should('not.be.visible')
 
     })
 
@@ -45,8 +55,9 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     /* TC005 - Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário */
 
-    it ('TC005 - Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
+    it('TC005 - Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
 
+    cy.clock()
     cy.get('#firstName').type('Eduardo')
     cy.get('#lastName').type('Filip')
     cy.get('#email').type('teste@email.com')
@@ -54,15 +65,15 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     cy.get('#open-text-area').type('Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste,Teste', {delay: 0})
     cy.get('button[type="submit"]').click()
 
-    cy.get('.error')
-    .should('be.visible')
+    cy.get('.error') .should('be.visible')
+    cy.tick(skipTime)
+    cy.get('.error').should('not.be.visible')
 
     })
 
-
 /* TC006 - Preencher e limpar os campos nome, sobrenome, email e telefone */
 
-    it ('TC006 - Preenche e limpa os campos nome, sobrenome, email e telefone', function(){
+    it('TC006 - Preenche e limpa os campos nome, sobrenome, email e telefone', function(){
     cy.get('#firstName')
     .type('Eduardo')
     .should('be.value','Eduardo')
@@ -88,20 +99,25 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     /* TC007 - Mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios */
 
     it('TC007 - Mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function(){
+
+        cy.clock()
         cy.contains('button','Enviar').click()
 
-        cy.get('.error')
-        .should('be.visible')
+        cy.get('.error').should('be.visible')
+        cy.tick(skipTime)
+        cy.get('.error').should('not.be.visible')
 
     })
-
 
     /* TC008 - Envia o formuário com sucesso usando um comando customizado */
 
     it('TC008 - Envia o formuário com sucesso usando um comando customizado', function(){
+        cy.clock()
         cy.fillMandatoryFieldsAndSubmit()
 
         cy.get('.success').should('be.visible')
+        cy.tick(skipTime)
+        cy.get('.success').should('not.be.visible')
 
     })
 
@@ -141,7 +157,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     })
 
-    /* TC012 - Seleciona um arquivo da pasta fixture */
+    /* TC012 - Seleciona um arquivo da pasta fixture Upload Click */
     it('TC012 - Seleciona um arquivo da pasta fixture',function(){
     cy.get('input[type="file"]')
     .should('not.have.value')
@@ -152,8 +168,8 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     })
 
-    /* TC013 - Seleciona um arquivo da pasta fixture */
-    it ('TC013 - Seleciona um arquivo da pasta fixture',function(){
+    /* TC013 - Seleciona um arquivo da pasta fixture - Drag and Drop */
+    it('TC013 - Seleciona um arquivo da pasta fixture',function(){
         cy.get('input[type="file"]')
         .should('not.have.value')
         .selectFile('./cypress/fixtures/example.json', { action: 'drag-drop' })
@@ -165,7 +181,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     /* TC014 - Seleciona um arquivo utilizando uma fixture para a qual foi dada um alias */
     
-    it ('TC014 - Seleciona um arquivo utilizando uma fixture para a qual foi dada um alias',function(){
+    it('TC014 - Seleciona um arquivo utilizando uma fixture para a qual foi dada um alias',function(){
         cy.fixture('example.json').as('sampleFile')
         cy.get('input[type="file"]')
         .should('not.have.value')
